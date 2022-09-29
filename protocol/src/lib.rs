sdroege/gst-plugin-rs

commit 5ebf9913a1c6d70f37c2c014b660baecca868d80

signalling: add and fix test for consumer disconnection
Fixes #92

signalling/src/handlers/mod.rs
@@ -120,7 +120,7 @@ impl Handler {
             .sessions
             .iter()
             .filter_map(|(session_id, session)| {
-                if session.producer == peer_id {
+                if session.producer == peer_id || session.consumer == peer_id {
                     Some(session_id.clone())
                 } else {
                     None
@@ -641,6 +641,57 @@ mod tests {
         tx.send(("consumer".to_string(), Some(message)))
             .await
             .unwrap();
+        let (peer_id, sent_message) = handler.next().await.unwrap();
+
+        assert_eq!(peer_id, "producer");
+        assert_eq!(
+            sent_message,
+            p::OutgoingMessage::EndSession(p::EndSessionMessage {
+                session_id: session_id
+            })
+        );
+    }
+
+    #[async_std::test]
+    async fn test_disconnect_consumer() {
+        let (mut tx, rx) = mpsc::unbounded();
+        let mut handler = Handler::new(Box::pin(rx));
+
+        new_peer(&mut tx, &mut handler, "producer").await;
+
+        let message = p::IncomingMessage::SetPeerStatus(p::PeerStatus {
+            roles: vec![p::PeerRole::Producer],
+            meta: None,
+            peer_id: None,
+        });
+        tx.send(("producer".to_string(), Some(message)))
+            .await
+            .unwrap();
+
+        new_peer(&mut tx, &mut handler, "consumer").await;
+
+        let message = p::IncomingMessage::StartSession(p::StartSessionMessage {
+            peer_id: "producer".to_string(),
+        });
+        tx.send(("consumer".to_string(), Some(message)))
+            .await
+            .unwrap();
+        let (peer_id, sent_message) = handler.next().await.unwrap();
+        assert_eq!(peer_id, "consumer");
+        let session_id = match sent_message {
+            p::OutgoingMessage::SessionStarted {
+                ref peer_id,
+                ref session_id,
+            } => {
+                assert_eq!(peer_id, "producer");
+                session_id.to_string()
+            }
+            _ => panic!("SessionStarted message missing"),
+        };
+
+        let _ = handler.next().await.unwrap();
+
+        tx.send(("consumer".to_string(), None)).await.unwrap();
         let (peer_id, sent_message) = handler.next().await.unwrap();
 
         assert_eq!(peer_id, "producer");

centricular/webrtcsink

commit 5582a7c56f15a344d3b36fe16c3be771277b4a7a

signalling: Do not stop consuming sessions when a peer stops being a producer
This is a regression from: https://github.com/centricular/webrtcsink/pull/93

protocol/src/lib.rs
@@ -45,6 +45,9 @@ pub enum PeerRole {
     /// Register as a listener
     #[serde(rename_all = "camelCase")]
     Listener,
+
+    #[serde(skip)]
+    All,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Default, Clone)]

signalling/src/handlers/mod.rs
@@ -86,8 +86,7 @@ impl Handler {
     }
 
     fn handle_peer_message(&mut self, peer_id: &str, peermsg: p::PeerMessage) -> Result<(), Error> {
-        let session_id = &peermsg.session_id;
-        let session = self
+        let session_id = &peermsg.session_id; let session = self
             .sessions
             .get(session_id)
             .context(format!("Session {} doesn't exist", session_id))?
@@ -115,16 +114,16 @@ impl Handler {
         Ok(())
     }
 
-    fn stop_producer(&mut self, peer_id: &str) {
+    fn stop_sessions(&mut self, peer_id: &str, role: p::PeerRole) {
         let sessions_to_end = self
             .sessions
             .iter()
-            .filter_map(|(session_id, session)| {
-                if session.producer == peer_id || session.consumer == peer_id {
+            .filter_map(|(session_id, session)| match role {
+                p::PeerRole::Producer | p::PeerRole::All if session.producer == peer_id => {
                     Some(session_id.clone())
-                } else {
-                    None
                 }
+                p::PeerRole::All if session.consumer == peer_id => Some(session_id.clone()),
+                _ => None,
             })
             .collect::<Vec<String>>();
 
@@ -144,7 +143,7 @@ impl Handler {
             _ => return,
         };
 
-        self.stop_producer(peer_id);
+        self.stop_sessions(peer_id, p::PeerRole::All);
 
         for (id, p) in self.peers.iter() {
             if !p.listening() {
@@ -215,7 +214,7 @@ impl Handler {
         }
 
         if old_status.producing() && !status.producing() {
-            self.stop_producer(peer_id);
+            self.stop_sessions(peer_id, p::PeerRole::Producer);
         }
 
         let mut status = status.clone();